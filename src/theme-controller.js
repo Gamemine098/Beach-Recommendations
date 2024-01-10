c65f8ee98babb53cd0f91f488a9fd01d81daf6de
gamemine <tanagameggth@gmail.com>
2024-01-10 18:30:56 +0700

    #

--- src/theme-controller.js
+++ src/theme-controller.js
@@ -2,9 +2,9 @@ const checkbox = document.getElementById("theme-controller");
 
 // Save checkbox state on change
 checkbox.addEventListener("change", (event) => {
-  localStorage.setItem("theme", event.target.checked ? "dark" : "light");  // Store "dark" or "light"
+  localStorage.setItem("theme", event.target.checked ? "sunset" : "emerald");  // Store "dark" or "light"
 });
 
 // Restore checkbox state on page load
 const savedState = localStorage.getItem("theme");
-checkbox.checked = savedState === "dark";  // Set checkbox based on saved state
+checkbox.checked = savedState === "sunset";  // Set checkbox based on saved state
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,29 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["light", "dark"],
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    themes: [
+      {
+        emerald: {
+          ...require("daisyui/src/theming/themes")["emerald"],
+          "--rounded-box": "1rem",
+          "--rounded-btn": "0.5rem",
+          "--rounded-badge": "1.9rem",
+          "--border-btn": "1px",
+          "--tab-border": "1px",
+          "--tab-radius": "0.5rem",
+        },
+        sunset: {
+          ...require("daisyui/src/theming/themes")["sunset"],
+          "--rounded-box": "1rem",
+          "--rounded-btn": "0.5rem",
+          "--rounded-badge": "1.9rem",
+          "--border-btn": "1px",
+          "--tab-border": "1px",
+          "--tab-radius": "0.5rem",
+        },
+      },
+    ],
+    darkTheme: "sunset", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
